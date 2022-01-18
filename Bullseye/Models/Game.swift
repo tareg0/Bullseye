5e4a5a246c0372265a7254757801f7ddb4d441eb
Taras Romanovskiy <3825014@gmail.com>
2022-01-18 17:06:09 +0200

    Initial Commit

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Taras Romanovskiy on 15.01.2022.
+//
+
+import Foundation
+struct Game {
+    var target = Int.random(in: 1...100)
+    var score = 0
+    var round = 1
+    
+    func points(sliderValue: Int) -> Int {
+        100 - abs(target - sliderValue)
+    }
+}
---
+++ Bullseye/Views/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Taras Romanovskiy on 15.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var alertIsVisible = false
+    @State private var sliderValue = 50.0
+    @State private var game = Game()
+    
+    var body: some View {
+        VStack {
+        Text("⚽️🏀🥎\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+                
+            Text(String(game.target))
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                
+            HStack {
+                Text("1")
+                    .bold()
+                Slider(value: $sliderValue, in: 1...100.0)
+            Text("100")
+                    .bold()
+            }
+            Button(action:{
+                print("Hello, SwiftUI!")
+                alertIsVisible = true
+            }){
+                Text("Hit me")
+            }
+            .alert(isPresented: $alertIsVisible,
+                   content: {
+                let roundedValue = Int(sliderValue.rounded())
+                return Alert(title: Text("Hello there!"), message: Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)), points this round."), dismissButton: .default(Text("Awersome!")))
+            })
+         }
+        }
+      }
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
